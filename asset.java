0bbb935f8caf0f9dd892f20f03795c9a430953bf
Christy Thom <christyraethom@gmail.com>
2019-03-04 15:19:31 -0800

    asset class created in class

---
+++ asset.java
@@ -0,0 +1,19 @@
+//represent financial assets
+
+public abstract class Asset {
+	
+	//fields
+	private double value;
+	
+	private double profit;
+	
+	//methods
+	public double getMarketValue() {
+		return value;
+	}
+	
+	public abstract void assetDescription();
+	
+	
+
+}
