a6928b5820234fc2f4168d7effd8cbf781d745de
Christy Thom <christyraethom@gmail.com>
2019-03-06 11:24:33 -0800

    Update UseProduct.java

--- UseProduct.java
+++ UseProduct.java
@@ -6,14 +6,16 @@ public class UseProduct {
 		Product acessory = new Product(2, "Shrug", 29.95);
 		Product boots = new Product(3, "Knee High Boots", 140);
 		Product stockings = new Product(4, "Stockings", 12.5);
+		Product necklace = new Product(5, "Necklace", 43);
 	
 		
 		//display results of product instantiation
-		System.out.println("All product info:");
+		System.out.println("My Product Wardrobe:");
 		System.out.println(dress.toString());
 		System.out.println(acessory.toString());
 		System.out.println(boots.toString());
 		System.out.println(stockings.toString());
+		System.out.println(necklace.toString());
 		
 		
 	}
